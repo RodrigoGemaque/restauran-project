40134b6120fce5826adb78bebb4ffcd665acb8fb
rodrigo <ro.gemaque16@gmail.com>
2021-07-20 23:18:36 -0300

    home view and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  get 'menu/index'
   devise_for :users
   resources :products
 
   root 'pages#home'
 
-  
+  get 'menu', to: "menu#index"  
   get 'catering', to: "pages#catering"
   get 'info', to: "pages#info"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
